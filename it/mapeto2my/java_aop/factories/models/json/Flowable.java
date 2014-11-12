8c1e8380c3d6827338a41a1482d15f7828748db2
FedMrt <federico.martena@gmail.com>
2014-11-12 10:16:17 +0100

    Create Flowable.java
    Commit of interface Flowable

---
+++ it/mapeto2my/java_aop/factories/models/json/Flowable.java
@@ -0,0 +1,17 @@
+package it.mapeto2my.java_aop.factories.models.json;
+
+import java.lang.reflect.Method;
+
+public interface Flowable {
+
+	Object intercept(Flow flow,Object target, Method targetMethod) throws Exception;
+	
+}package it.mapeto2my.java_aop.factories.models.json;
+
+import java.lang.reflect.Method;
+
+public interface Flowable {
+
+	Object intercept(Flow flow,Object target, Method targetMethod) throws Exception;
+	
+}
