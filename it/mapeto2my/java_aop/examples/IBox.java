bdec55efeadd03e98a0ff1c3d846238ba1445259
FedMrt <federico.martena@gmail.com>
2014-11-12 16:41:04 +0100

    sync commit

--- it/mapeto2my/java_aop/examples/IBox.java
+++
@@ -1,7 +0,0 @@
-package it.mapeto2my.java_aop.examples;
-
-public interface IBox {
-
-	String info();
-	
-}
--- it/mapeto2my/java_aop/factories/IFactory.java
+++
@@ -1,7 +0,0 @@
-package it.mapeto2my.java_aop.factories;
-
-public interface IFactory {
-
-	Object getTarget(String key) throws Exception;
-	
-}
