49f99fbf1f8494e1e49ff11d9323b66fd4cc64af
FedMrt <federico.martena@gmail.com>
2014-11-12 10:11:16 +0100

    Delete Flowable.java

--- it/pepo/java_aop/factories/models/json/Flowable.java
+++
@@ -1,9 +0,0 @@
-package it.pepo.java_aop.factories.models.json;
-
-import java.lang.reflect.Method;
-
-public interface Flowable {
-
-	Object intercept(Flow flow,Object target, Method targetMethod) throws Exception;
-	
-}
